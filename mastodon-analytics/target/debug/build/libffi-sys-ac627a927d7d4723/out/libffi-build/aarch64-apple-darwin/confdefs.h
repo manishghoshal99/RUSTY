9a8406ffbae28d726d26d5e59b2057442bdea45f
manishghoshal99 <meloidasdragneel12@gmail.com>
2025-03-30 21:56:33 +1100

    change repo

--- mastodon-analytics/target/debug/build/libffi-sys-ac627a927d7d4723/out/libffi-build/aarch64-apple-darwin/confdefs.h
+++
@@ -1,9 +0,0 @@
-/* confdefs.h */
-#define PACKAGE_NAME "libffi"
-#define PACKAGE_TARNAME "libffi"
-#define PACKAGE_VERSION "3.4.4"
-#define PACKAGE_STRING "libffi 3.4.4"
-#define PACKAGE_BUGREPORT "http://github.com/libffi/libffi/issues"
-#define PACKAGE_URL ""
-#define PACKAGE "libffi"
-#define VERSION "3.4.4"
